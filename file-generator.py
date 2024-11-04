c9a2247dfab84dadf4c0880a171f4bf8c03480de
SmerfTV <stradczukm@gmail.com>
2024-11-04 21:07:36 +0100

    dodanie parametrow

--- file-generator.py
+++ file-generator.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import shutil
+import argparse
+
 
 def generateFiles(miesiace, dni_tygodnia, pory_dnia):
     k = 0
@@ -15,13 +17,15 @@ def generateFiles(miesiace, dni_tygodnia, pory_dnia):
         for dzien in dni:
             if (k < len(pory_dnia)):
                 pora = 'rano' if pory_dnia[k] == 'r' else 'wieczór'
-            else: pora = 'rano'
+            else:
+                pora = 'rano'
             k = k + 1
             sciezka = os.path.join(miesiac, dzien, pora)
             os.makedirs(sciezka, exist_ok=True)
             print(f"Utworzono katalog: {sciezka}")
 
-#funkcja tylko do testów
+
+# Funkcja tylko do testów
 def deleteFiles():
     miesiace = ["styczeń", "luty", "marzec", "kwiecień", "maj",
                 "czerwiec", "lipiec", "sierpień", "wrzesień",
@@ -30,8 +34,43 @@ def deleteFiles():
         if os.path.exists(miesiac):
             shutil.rmtree(miesiac)
 
+
 def main():
-    generateFiles(["styczeń", "luty", "kwiecień"], ["pn-wt", "pt", "sb"], ["r","w",'r','r'])
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Skrypt do tworzenia lub odczytu plików.')
+
+    # a) Wybór miesięcy (można wybrać dowolną ilość)
+    parser.add_argument('--months', nargs='+', required=True, help='Lista miesięcy (np. styczeń luty)')
+
+    # b) Wybór zakresu dni tygodnia (tyle samo argumentów co w (a) )
+    parser.add_argument('--days', nargs='+', required=True, help='Lista zakresów dni tygodnia (np. pn-pt wt-śr)')
+
+    # c) Wybór albo rano albo wieczorem (domyślnie rano)
+    parser.add_argument('--pory', nargs='*', help='Lista pór dnia (r lub w). Jeśli nie podano, domyślnie "rano"')
+
+    # d) Wybór opcji tworzenie / odczyt
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-t', '--tworzenie', action='store_true', help='Tworzenie plików')
+    group.add_argument('-o', '--odczyt', action='store_true', help='Odczyt plików')
+
+    # e) Wybór formatu pliku csv / json
+    parser.add_argument('-c', '--csv', action='store_true', help='Użyj formatu CSV')
+    parser.add_argument('-j', '--json', action='store_true', help='Użyj formatu JSON')
+
+    args = parser.parse_args()
+
+    miesiace = args.months
+    dni_tygodnia = args.days
+    pory_dnia = args.pory if args.pory else []
+
+    if len(miesiace) != len(dni_tygodnia):
+        print('Liczba miesięcy musi być równa liczbie zakresów dni tygodnia.')
+        sys.exit(1)
+
+    generateFiles(miesiace, dni_tygodnia, pory_dnia)
     deleteFiles()
 
-main()
+
+if __name__ == '__main__':
+    main()
